43fdc0c6972302b613c4331f1711ba9fe42b2abc
Nadia Khoptiuk <holineinadia@gmail.com>
2022-06-05 18:40:59 +0300

    add task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,32 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', startChangeColors);
+refs.stopBtn.addEventListener('click', stopChangeColors);
+refs.stopBtn.setAttribute('disabled', '');
+
+// функція, яка генерує рандомний колір
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// функція, яка починає змінювати кольори фону
+function startChangeColors(evt) {
+  evt.target.setAttribute('disabled', '');
+  refs.stopBtn.removeAttribute('disabled');
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+// функція, яка зупиняє зміну кольору фону
+function stopChangeColors(evt) {
+  clearInterval(intervalId);
+  evt.target.setAttribute('disabled', '');
+  refs.startBtn.removeAttribute('disabled');
+}
